9f82137ec9b378d97a11f09df806b0984d16ba39
CharlsCuerbo <boboy.cuerbo@gmail.com>
2025-02-22 10:01:29 +0800

    graph of omega

---
+++ main.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x = [2,4,4,3.5,3,3.5,4.5, 5 ,5.5, 6.5,7,6.5,6,6,8]
+y = [2,2,3,3.5,5,6.5,7.5, 7.5 ,7.5,6.5,5,3.5,3,2,2]
+
+plt.plot(x,y, label = "linya")
+
+plt.xlabel( 'pabaybag')
+plt.ylabel("pasaka")
+
+plt.show()
